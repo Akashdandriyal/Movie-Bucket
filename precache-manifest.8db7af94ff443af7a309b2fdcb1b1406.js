43a438fc5259e04b187cb5c571dd004668f87cb2
Akash Dandriyal <akash.d1409@gmail.com>
2020-09-16 20:14:47 +0530

    Updates

---
+++ precache-manifest.8db7af94ff443af7a309b2fdcb1b1406.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b06d19a035e1a29b35308a95809f8bd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd78c4138bf838946d2d",
+    "url": "/static/css/main.91f48a39.chunk.css"
+  },
+  {
+    "revision": "ead9c61ede8b126a97ed",
+    "url": "/static/js/2.52ac47a5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.52ac47a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd78c4138bf838946d2d",
+    "url": "/static/js/main.fabd9074.chunk.js"
+  },
+  {
+    "revision": "de0d39dbac3736de8ef6",
+    "url": "/static/js/runtime-main.48f5a834.js"
+  }
+]);
